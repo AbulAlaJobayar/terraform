ac9f91b396470bceb79062b997d110f92bdb40c2
AbulAlaJobayar <jobayar59@gmail.com>
2026-03-13 22:59:16 +0600

    terraform registry

--- main.tf
+++ main.tf
@@ -113,25 +113,49 @@
 
 
 # workspace
+# provider "aws" {
+#   region = var.aws_region
+# }
+
+# data "aws_ami" "ubuntu" {
+#   most_recent  =true
+#   owners = ["amazon"]
+#   filter {
+#     name = "name"
+#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#   }
+# }
+
+# resource "aws_instance" "myec2" {
+#   ami =data.aws_ami.ubuntu.id
+#   instance_type = var.instance_type
+#   tags = {
+#     Name= "EC2-${terraform.workspace}"
+#     Environment=terraform.workspace
+#   }
+# }
+
+# terraform registry
+
 provider "aws" {
   region = var.aws_region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent  =true
-  owners = ["amazon"]
-  filter {
-    name = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-resource "aws_instance" "myec2" {
-  ami =data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
   tags = {
-    Name= "EC2-${terraform.workspace}"
-    Environment=terraform.workspace
+    Terraform = "true"
+    Environment = "dev"
   }
 }
-
